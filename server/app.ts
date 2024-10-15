a94c0aceadb7960c8ed0e83300021f88448ce7d8
ashi-kamra <ak118@wellesley.edu>
2024-10-14 17:40:50 -0700

    updating changes

--- server/app.ts
+++ server/app.ts
@@ -3,9 +3,23 @@ import FriendingConcept from "./concepts/friending";
 import PostingConcept from "./concepts/posting";
 import SessioningConcept from "./concepts/sessioning";
 
+//NEW concepts
+import ConnectingConcept from "./concepts/connecting";
+import ConsentSurveyConcept from "./concepts/consentSurvey";
+import LabelConcept from "./concepts/labelling";
+import MessageConcept from "./concepts/messaging";
+import UserConcept from "./concepts/user";
+
 // The app is a composition of concepts instantiated here
 // and synchronized together in `routes.ts`.
 export const Sessioning = new SessioningConcept();
 export const Authing = new AuthenticatingConcept("users");
-export const Posting = new PostingConcept("posts");
 export const Friending = new FriendingConcept("friends");
+export const Posting = new PostingConcept("posts");
+
+//NEW
+export const User = new UserConcept("users");
+export const Messaging = new MessageConcept("messages");
+export const Labelling = new LabelConcept("labeled");
+export const Consenting = new ConsentSurveyConcept("content"); //change title
+export const Connecting = new ConnectingConcept("connections");
