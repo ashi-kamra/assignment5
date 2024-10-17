ee6ed66299171b3fe3669525ce93439c06a4f090
ashi-kamra <ak118@wellesley.edu>
2024-10-17 17:37:05 -0400

    fixing connection function

--- client/components/Connector/ConnectorForm.vue
+++ client/components/Connector/ConnectorForm.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
+import router from "@/router";
 import { useUserStore } from "@/stores/user";
 import { ref } from "vue";
 
 const connection_id = ref("");
 
-const { makeConnection } = useUserStore();
+const { makeConnection, updateSession } = useUserStore();
 
 async function connection() {
-  await makeConnection(connection_id);
+  await makeConnection(connection_id.value);
   await updateSession();
   void router.push({ name: "Home" });
 }
